c3090819a7461d4cbc4e099cd51244ec7684429a
biqqles <biqqles@protonmail.com>
2018-08-10 01:43:46 +0100

    Refactor main activity

--- app/src/main/java/eu/biqqles/nextlit/MainActivity.java
+++ app/src/main/java/eu/biqqles/nextlit/MainActivity.java
@@ -38,14 +38,22 @@ import android.widget.ToggleButton;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
                           implements NavigationView.OnNavigationItemSelectedListener,
                                      SharedPreferences.OnSharedPreferenceChangeListener {
-
+    // A navigation drawer that allows the service to be enabled and disabled, patterns to be
+    // previewed and a default pattern to be set.
     private LedControl ledcontrol;
     private SharedPreferences prefs;
     private Resources resources;
+
+    private DrawerLayout navigationDrawer;
+    private NavigationView navigationView;
+    private Spinner patternSpinner;
+    private ToggleButton previewButton;
+    private Switch serviceSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,105 +64,104 @@ public class MainActivity extends AppCompatActivity
         resources = getResources();
 
         final PatternProvider patternProvider = new PatternProvider(getApplicationContext());
+
         try {
             ledcontrol = new LedControl(patternProvider.patterns);
         } catch (IOException e) {
             // root access denied/unavailable
             rootDenied();
+            return;
         }
 
         // ensure that the service is aware of all changes to settings
+        // (see onSharedPreferenceChanged)
         prefs.registerOnSharedPreferenceChangeListener(this);
         getSharedPreferences("apps_enabled", MODE_PRIVATE)
                 .registerOnSharedPreferenceChangeListener(this);
         getSharedPreferences("apps_patterns", MODE_PRIVATE)
                 .registerOnSharedPreferenceChangeListener(this);
 
-        // set up navigation drawer
-        final NavigationView navigationView = findViewById(R.id.nav_view);
+        // set up navigation menu...
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationDrawer = findViewById(R.id.drawer_layout);
 
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         final ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open,
+                this, navigationDrawer, toolbar, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        navigationDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        // and header
+        // ...and header
         final View header = navigationView.getHeaderView(0);
-        final Switch serviceSwitch = header.findViewById(R.id.serviceSwitch);
-        final Spinner patternSpinner = header.findViewById(R.id.patternSpinner);
-        final ToggleButton previewButton = header.findViewById(R.id.previewButton);
         final TextView subtitle = header.findViewById(R.id.service_state);
+        serviceSwitch = header.findViewById(R.id.serviceSwitch);
+        patternSpinner = header.findViewById(R.id.patternSpinner);
+        previewButton = header.findViewById(R.id.previewButton);
 
         // this switch enables and disables the notification service
+        serviceSwitch.setChecked(prefs.getBoolean("service_enabled", false));
+
         serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton button, boolean checked) {
-            int statusText;
+                int statusText;
 
-            if (checked) {
-                // take user to Notification access if they haven't enabled the service already
-                if (!serviceBound()) {
-                    startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
-                    Toast.makeText(MainActivity.this,
-                                   resources.getString(R.string.enable_service), Toast.LENGTH_SHORT)
-                            .show();
+                if (checked) {
+                    // take user to Notification access if they haven't enabled the service already
+                    if (!serviceBound()) {
+                        showNotificationListenerSettings();
+                    }
+                    statusText = R.string.service_enabled;
+                } else {
+                    statusText = R.string.service_disabled;
                 }
 
+                Toast.makeText(MainActivity.this, statusText, Toast.LENGTH_SHORT).show();
+                subtitle.setText(statusText);
+
                 /* Normal procedure here would be to call start/stopService, but for whatever reason
-                Android ignores these calls for NotificationListenerServices, and so effectively
-                the only thing that governs whether a service is enabled or not is if it's enabled
-                in Notification access. Instead, we accept that the service will always run and
-                just modify a static flag that says if the lights should be enabled or not. */
-                statusText = R.string.service_enabled;
-            } else {
-                statusText = R.string.service_disabled;
-            }
-
-            Toast.makeText(MainActivity.this, statusText, Toast.LENGTH_SHORT).show();
-            subtitle.setText(statusText);
-
-            prefs.edit().putBoolean("service_enabled", checked).apply();
+                Android ignores these calls for NotificationListenerServices, and so effectively the
+                only thing that governs whether a service is enabled or not is if it's enabled in
+                Notification access. Instead, we accept that the service will always run and just
+                modify a static flag that determines if the lights should be enabled or not. */
+                prefs.edit().putBoolean("service_enabled", checked).apply();
             }
         });
 
-        serviceSwitch.setChecked(prefs.getBoolean("service_enabled", false));
-
-        // populate spinner
+        // this spinner allows the default pattern to be selected
+        ArrayList<String> patternNames = patternProvider.getNames();
+        patternNames.set(0, resources.getString(R.string.choose_pattern));
         patternSpinner.setAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_dropdown_item, patternProvider.getNames()));
+                android.R.layout.simple_spinner_dropdown_item, patternNames));
+
+        patternSpinner.setSelection(patternProvider.indexOf(prefs.getString("pattern_name", null)));
 
         patternSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView adapterView, View view, int i, long l) {
                 // halt preview and update preferences
-                previewButton.setChecked(false);
-                final String patternName = patternSpinner.getSelectedItem().toString();
+                stopPreview(previewButton);
+                // no pattern selected is represented by null
+                String patternName = i > 0 ? patternSpinner.getSelectedItem().toString() : null;
                 prefs.edit().putString("pattern_name", patternName).apply();
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) { }
         });
 
-        patternSpinner.setSelection(patternProvider.indexOf(prefs.getString("pattern_name", null)));
+        // this toggle allows the pattern currently selected in the spinner to be previewed
         previewButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton button, boolean checked) {
-            if (checked) {
-                final String patternName = patternSpinner.getSelectedItem().toString();
-
-                ledcontrol.clearAll();
-                ledcontrol.setPattern(patternName);
-
-                final String message = MessageFormat.format(
-                        resources.getString(R.string.preview_active), patternName);
-                Snackbar.make(header, message, Snackbar.LENGTH_LONG).show();
-            } else {
-                restoreLightsState();
-            }
+                if (button.isPressed()) {  // ignore calls from setChecked and the like
+                    if (checked) {
+                        startPreview(button, patternSpinner.getSelectedItem().toString());
+                    } else {
+                        stopPreview(button);
+                    }
+                }
             }
         });
 
@@ -164,10 +171,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        // Handle back button pressed.
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        // Closes navigation drawer on back button press.
+        if (navigationDrawer.isDrawerOpen(GravityCompat.START)) {
+            navigationDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -175,7 +181,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks.
+        // Handles navigation menu item clicks.
         final int id = item.getItemId();
 
         if (id == R.id.nav_settings) {
@@ -190,6 +196,9 @@ public class MainActivity extends AppCompatActivity
                     break;
             }
 
+            // select fragment in menu
+            navigationView.setCheckedItem(id);
+            // display fragment
             getSupportFragmentManager()
                     .beginTransaction()
                     .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
@@ -197,8 +206,7 @@ public class MainActivity extends AppCompatActivity
                     .commitNow();
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        navigationDrawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
@@ -209,11 +217,11 @@ public class MainActivity extends AppCompatActivity
         restoreLightsState();
     }
 
-    private void restoreLightsState() {
+    public void restoreLightsState() {
         // Restores the "proper" state of the leds. Should be called after any setting which might
         // require a change in their current visibility has been modified.
         if (serviceBound()) {
-            Intent serviceIntent = new Intent(this, NotificationLightsService.class);
+            final Intent serviceIntent = new Intent(this, NotificationLightsService.class);
             serviceIntent.addCategory("restore_state");
             startService(serviceIntent);
             stopService(serviceIntent);
@@ -222,7 +230,25 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    boolean serviceBound() {
+    public void startPreview(CompoundButton button, String patternName) {
+        // Begins previewing the given pattern.
+        button.setChecked(true);
+
+        ledcontrol.clearAll();
+        ledcontrol.setPattern(patternName);
+
+        final String message = MessageFormat.format(resources.getString(R.string.preview_active),
+                patternName);
+        Snackbar.make(navigationView, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public void stopPreview(CompoundButton button) {
+        // Halts any ongoing preview.
+        button.setChecked(false);
+        restoreLightsState();
+    }
+
+    private boolean serviceBound() {
         // Reports whether the notification service has been bound (i.e. activated).
         final String enabledNotificationListeners = Settings.Secure.getString(
                 getContentResolver(),"enabled_notification_listeners");
@@ -231,12 +257,19 @@ public class MainActivity extends AppCompatActivity
                 !enabledNotificationListeners.contains(getPackageName()));
     }
 
-    void rootDenied() {
-        // Handle root access unavailable or denied.
-        Toast.makeText(this, resources.getString(R.string.root_denied), Toast.LENGTH_LONG)
-                .show();
+    private void rootDenied() {
+        // Handles root access unavailable or denied.
+        Toast.makeText(this, resources.getString(R.string.root_denied), Toast.LENGTH_LONG).show();
         // prevent the service from constantly trying to restart itself if already enabled
         prefs.edit().putBoolean("service_enabled", false).apply();
         finish();
+    }
+
+    private void showNotificationListenerSettings() {
+        // Presents the user with the "Notification access" settings screen and asks them to give
+        // the service access.
+        startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
+        Toast.makeText(MainActivity.this,
+                resources.getString(R.string.enable_service), Toast.LENGTH_SHORT).show();
     }
 }
